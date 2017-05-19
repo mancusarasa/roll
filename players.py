145ac7841c2160e94a05d342c677c3c8154db34f
Pablo Salinas <pablosalinas@suremptec.com.ar>
2017-05-19 16:51:54 -0300

    Stupid comments on how to simulate jumps and gravity

--- players.py
+++ players.py
@@ -12,6 +12,15 @@ class FirstPlayer(object):
         '''
         super(FirstPlayer, self).__init__()
         self.ball = Ball(10)
+        # idea: to perform a jump, set this (initial) speed to a 
+        # convenient value. then, update the speed with this formula:
+        # self.__speed_y += GRAVITY
+        # self.rect.y += self.__speed_y
+        self.__speed_y = 0
+        # this boolean indicates if the player is in the middle of a 
+        # jump, to avoid jumping twice on the same keystroke. set to
+        # True when starting a jump; set to False on collision with something
+        self.__midjump = False
 
     def move_right(self):
         '''
