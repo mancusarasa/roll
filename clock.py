967ae9443c3d5b8a5e9025653bedff5129eb3ab4
Pablo Salinas <pablosalinas@suremptec.com.ar>
2017-05-24 12:12:54 -0300

    Adds the Clock class

---
+++ clock.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Clock(object):
+    '''
+    A ticking clock. Provides a single method to get
+    the elapsed time.
+    '''
+    def __init__(self):
+        '''
+        Class constructor.
+        '''
+        self.__clock = pygame.time.Clock()
+
+    def tick(self):
+        '''
+        Returns the elapsed time between to consecutive calls
+        of the tick() method.
+        @return ellapsed_time.
+        '''
+        return self.__clock.tick()
