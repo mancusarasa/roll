f51f018846115754dc4b193dff97bc71248271a8
Pablo Salinas <pablo.salinas@avature.net>
2020-01-17 15:02:58 -0300

    Randomly place a collection of beams across the screen

--- beam_collection.py
+++ beam_collection.py
@@ -1,15 +1,25 @@
 from beam import Beam
+from game_config import GameConfig
+from random import randint
 
 
 class BeamCollection(object):
     '''
     The collection of beams.
     '''
-    def __init__(self, beams_count=3):
+    def __init__(self, beams_count=10):
         '''
         Constructor
         '''
         super(BeamCollection, self).__init__()
+        config = GameConfig()
+        width = int(config.get('beams', 'width'))
+        height = int(config.get('beams', 'height'))
+        screen_width = int(config.get('screen', 'width'))
+        screen_height = int(config.get('screen', 'height'))
+
         for i in range(beams_count):
-            b = Beam(60, 20, i*100 + 100, i*100 +100)
+            x_offset = randint(0, screen_width)
+            y_offset = i*(screen_height/beams_count)
+            b = Beam(width, height, x_offset, y_offset)
         
